069948e3cd3b87426b21200558401223ca3c9178
lericardolima <lericardolima@gmail.com>
2020-04-17 10:19:32 -0300

    Refatorar a criação do servidor.

--- src/app.js
+++ src/app.js
@@ -1,14 +1,23 @@
+// Criar app.
 const app = require('express')();
-const http = require('http')
-const cors = require('cors');
 
-app.use(cors());
+// Configurar porta do servidor.
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
 
 const port = process.env.PORT || 8080;
 app.set('port', port);
 
+// Configurar CROS.
+const cors = require('cors');
+app.use(cors());
+
+// Configurar Swagger.
 require('./config/swagger/swagger')(app);
 
+// Criar e iniciar o servidor.
+const http = require('http')
 const server = http.createServer(app);
 server.listen(port);
 
