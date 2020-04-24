272da82a7908f6fb98d47b867a04560d42f183d5
lericardolima <lericardolima@gmail.com>
2020-04-23 22:22:41 -0300

    Refatorar estrutura da documentação da API.

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,10 @@ const usersRouter = require('./user');
 const loginRouter = require('./login');
 
 module.exports = (app) => {
+  // Mapeamento das rotas para os recursos da API.
   app.use('/api/login', loginRouter);
   app.use('/api/users', usersRouter);
+
+  // Redirecionar requisições não mapeadas para a página do Swagger.
+  app.get('*', (req, res) => res.redirect('/api/swagger-ui'));
 };
